e2a85233b1c16c1c3ccbb712f4d2bddb1b477099
rohan-naik <rohan.naik@contentstack.com>
2022-04-24 20:48:20 +0530

    Adding code to countrer

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Counter() {
+
+    const [counter, setCounter] = useState(0);
+
+    const handleButtonclick = () => {
+        setCounter(counter+1);
+    }
+
   return (
-    <div>Counter</div>
+      <>
+        <div>Counter</div>
+        <div>
+            <button onClick={handleButtonclick}>Increment Counter</button>
+        </div>
+        <div>
+            Counter value is at : {counter}
+        </div>
+      </>
   )
 }
 
